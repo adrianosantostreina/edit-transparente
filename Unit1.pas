7c2effe803486db93b04af52db909a6c9dca04bf
Adriano Santos <asrsantos@gmail.com>
2018-09-07 23:05:01 -0300

    TDevRocks Tira-Teima

---
+++ Unit1.pas
@@ -0,0 +1,29 @@
+unit Unit1;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Objects,
+  FMX.Layouts, FMX.Controls.Presentation, FMX.Edit;
+
+type
+  TForm1 = class(TForm)
+    Edit1: TEdit;
+    StyleBook1: TStyleBook;
+    Layout1: TLayout;
+    Rectangle1: TRectangle;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+end.
